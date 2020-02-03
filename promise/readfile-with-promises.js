1db3a1151be50b1d2f2501d47e3a11d77ecfc5bf
kwon <kkdosk@naver.com>
2020-02-03 14:31:10 +0900

    this binding

--- promise/readfile-with-promises.js
+++ promise/readfile-with-promises.js
@@ -1,5 +1,41 @@
 const fs = require('fs-extra');
 
 const countLinesWithText = function(pathToFile) {
-  fs.readFile(pathToFile);
+  fs.readFile(pathToFile)
+    .then(content => content.toString())
+    .then(content => content.split('\n'))
+    .then(lines => lines.filter(line => line.includes('THIS LINE')))
+    .then(lines => lines.length)
+    .then(count => checkLineExists(count))
+    .then(count => console.log(`Number of lines with THIS LINE is ${count}`))
+    .catch(error => console.log(`ERROR: ${pathToFile}, ${error.message}`));
 };
+
+const checkLineExists = function(count) {
+  if (count === 0) {
+    throw new Error('text does not exist in file');
+  }
+  return count;
+};
+
+countLinesWithText('readfile-with-promises.js');
+countLinesWithText('readfile.js');
+countLinesWithText('invalid');
+
+class App {
+  constructor() {
+    console.log(this);
+    this.a = 5;
+  }
+
+  data() {
+    console.log(this.a);
+  }
+
+  render() {
+    return this.data.bind(this);
+  }
+}
+
+var a = new App();
+console.log(a.render()());
