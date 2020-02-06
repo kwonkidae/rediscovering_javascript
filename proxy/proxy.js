1f4306de714e5d2a28f2fc82ca798f8b48abb926
kwon <kkdosk@naver.com>
2020-02-06 13:27:24 +0900

    proxy get receiver

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -16,3 +16,43 @@ const a = {
 };
 
 a.b('kkdosk');
+
+var proxy = new Proxy(
+  {
+    a: 1,
+  },
+  {
+    get: function(target, property, receiver) {
+      // console.log(target, property, receiver);
+      return { target, property, receiver };
+    },
+    set: function(target, property, receiver) {
+      console.log({ target, property, receiver });
+      return { target, property, receiver };
+    },
+  }
+);
+
+proxy.a = 5;
+
+let user = {
+  _name: 'Guest',
+  get name() {
+    return this._name;
+  },
+};
+
+let userProxy = new Proxy(user, {
+  get(target, prop, receiver) {
+    return Reflect.get(...arguments);
+  },
+});
+
+let admin = {
+  __proto__: userProxy,
+  _name: 'Admin',
+};
+
+console.log(admin.name);
+
+// console.log(Reflect.get(proxy, 'getReceiver', { a: 1, b: 1 }));
