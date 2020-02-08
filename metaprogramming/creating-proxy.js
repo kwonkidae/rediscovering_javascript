598e51c9a16a02f6d76740e1204c1336cc1ad9fb
kwon <kkdosk@naver.com>
2020-02-09 01:25:24 +0900

    proxy trap

---
+++ metaprogramming/creating-proxy.js
@@ -0,0 +1,36 @@
+class Employee {
+  constructor(firstName, lastName, yearOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+  }
+
+  get fullname() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  get age() {
+    return new Date().getFullYear() - this.yearOfBirth;
+  }
+}
+
+const printInfo = function(employee) {
+  console.log(`First name: ${employee.firstName}`);
+  console.log(`Fullname: ${employee.fullname}`);
+  console.log(`Age: ${employee.age}`);
+};
+
+const john = new Employee('John', 'Doe', 2010);
+
+const handler = {
+  get: function(target, propertyName, receiver) {
+    if (propertyName === 'firstName') {
+      console.log(`target is john? ${john === target}`);
+      console.log(`propertyName is ${propertyName}`);
+      console.log(`receiver is proxyDoe? ${proxyDoe === receiver}`);
+    }
+    return Reflect.get(target, propertyName);
+  },
+};
+const proxyDoe = new Proxy(john, handler);
+printInfo(proxyDoe);
